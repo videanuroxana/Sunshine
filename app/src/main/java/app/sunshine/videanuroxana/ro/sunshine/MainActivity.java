7f882cb73207e13b674ae69e8ae8bd874244be2c
Roxana Videanu <roxanavasilenico@yahoo.com>
2015-02-14 17:12:59 +0200

    -- initial commit

---
+++ app/src/main/java/app/sunshine/videanuroxana/ro/sunshine/MainActivity.java
@@ -0,0 +1,164 @@
+package app.sunshine.videanuroxana.ro.sunshine;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import static app.sunshine.videanuroxana.ro.sunshine.R.id.list_view_forecast;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+        //a43bb0685ea3c79aec54bca626c1ee43(weather api key)
+           // http://api.openweathermap.org/data/2.5/forecast/daily?q=Pitesti&mode=json&units=metric&cnt=7
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            List<String> items = new ArrayList<String>();
+            items.add("Luni 09 Feb Sunny 23");
+            items.add("Marti 10 Feb Sunny 24");
+            items.add("Miercuri 11 Feb Sunny 25");
+            items.add("Joi 12 Feb Sunny 26");
+            items.add("Vineri 13 Feb Rain 23");
+            items.add("Sambata 14 Feb Snow 23");
+            items.add("Duminica 15 Feb Sunny 13");
+
+
+            ArrayAdapter<String> adaptor = new ArrayAdapter<String>(getActivity(),
+                                                                     R.layout.list_item_forecast,
+                                                                     R.id.list_item_forecast_textview,
+                                                                     items);
+
+
+
+            ListView lv = (ListView) rootView.findViewById(R.id.list_view_forecast);
+            lv.setAdapter(adaptor);
+
+// These two need to be declared outside the try/catch
+// so that they can be closed in the finally block.
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+// Will contain the raw JSON response as a string.
+            String forecastJsonStr = null;
+
+            try {
+                // Construct the URL for the OpenWeatherMap query
+                // Possible parameters are available at OWM's forecast API page, at
+                // http://openweathermap.org/API#forecast
+
+                String loc = "London";
+                String urlStr = "http://api.openweathermap.org/data/2.5/forecast/daily?q="+loc+"&mode=json&units=metric&cnt=7";
+                URL url = new URL(urlStr);
+
+                // Create the request to OpenWeatherMap, and open the connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    forecastJsonStr = null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    forecastJsonStr = null;
+                }
+                forecastJsonStr = buffer.toString();
+            } catch (IOException e) {
+                Log.e("PlaceholderFragment", "Error ", e);
+                // If the code didn't successfully get the weather data, there's no point in attempting
+                // to parse it.
+                forecastJsonStr = null;
+            } finally{
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e("PlaceholderFragment", "Error closing stream", e);
+                    }
+                }
+            }
+
+
+            return rootView;
+        }
+    }
+}
